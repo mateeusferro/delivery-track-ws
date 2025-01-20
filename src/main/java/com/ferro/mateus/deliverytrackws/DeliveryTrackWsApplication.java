7aebe6412fd0416eaa65dd680e1ccf24536a3a13
mateus <mateus.03fernandesaraujo@gmail.com>
2025-01-20 19:39:04 -0300

    init: initial commit

---
+++ src/main/java/com/ferro/mateus/deliverytrackws/DeliveryTrackWsApplication.java
@@ -0,0 +1,13 @@
+package com.ferro.mateus.deliverytrackws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryTrackWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferro/mateus/deliverytrackws/DeliveryTrackWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferro.mateus.deliverytrackws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
